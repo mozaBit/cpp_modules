a92e4b1cd6badcf36011a65fc92ab9baa43d386b
mozaBit <bamhammedmetehri@gmail.com>
2024-11-19 10:24:41 +0100

    in the middle of cpp04

---
+++ cpp_module_04/ex03/AMateria.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmetehri <bmetehri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 09:42:32 by bmetehri          #+#    #+#             */
+/*   Updated: 2024/11/19 10:22:03 by bmetehri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const &type) : _type(type)
+{
+}
+
+std::string const &AMateria::getType() const
+{
+	return this->_type;
+}
+
+void AMateria::use(ICharacter &target)
+{
+	std::cout << target.getName() << " is using BaseAMateria" << std::endl;
+}
