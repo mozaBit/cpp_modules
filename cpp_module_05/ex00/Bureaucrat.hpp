d60a4e77f85c0ee79a4c1e47cfa775c832bf8a01
mozaBit <bamhammedmetehri@gmail.com>
2024-12-30 19:02:36 +0100

    started cpp_module05

---
+++ cpp_module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmetehri <bmetehri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/30 18:50:37 by bmetehri          #+#    #+#             */
+/*   Updated: 2024/12/30 18:54:29 by bmetehri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __BUREAUCRAT_HPP__
+# define __BUREAUCRAT_HPP__
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat {
+	private:
+		const std::string	_name;
+		int					_grade;
+	public:
+};
+
+
+#endif
+
